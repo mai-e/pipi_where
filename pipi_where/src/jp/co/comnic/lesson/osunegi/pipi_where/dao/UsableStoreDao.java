e7fb53528b31d8331adaa44c525a78f8875d678b
COMNIC02 <COMNIC02@DESKTOP-CG4JDFG>
2019-06-21 15:35:02 +0900

    a

--- pipi_where/src/jp/co/comnic/lesson/osunegi/pipi_where/dao/UsableStoreDao.java
+++ pipi_where/src/jp/co/comnic/lesson/osunegi/pipi_where/dao/UsableStoreDao.java
@@ -11,18 +11,22 @@ import javax.naming.NamingException;
 public class UsableStoreDao {
 
 	public static ArrayList<String> findBy(String name) throws DaoException{
-		String sql = "SELECT * FROM card";
+		String sql = "SELECT card.card_name, store.store_jpn FROM card, usable_store, store"
+				+"WHERE card.card_name = usable_store.card_name AND usable_store.store_id = store.store_id"
+				+"AND card.card_name = ?";
+		ArrayList<String> usableStoreList = new ArrayList<String>();
 		try (Connection conn = ConnectionFactory.getConnection();
 				 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-				
+				pstmt.setString(1, name);
 				
 				try (ResultSet rs = pstmt.executeQuery()) {
 					while (rs.next()) {
+						usableStoreList.add(rs.getString("store_jpn"));
 					}
 				}
 			} catch (NamingException | SQLException e) {
 				throw new DaoException(e);
 			}
-		return null;
+		return usableStoreList;
 	}
 }
