8b538f1fb764f9151c8dc2d9da0627466c7a2e8f
COMNIC02 <COMNIC02@DESKTOP-CG4JDFG>
2019-06-21 13:13:48 +0900

    e

---
+++ pipi_where/src/jp/co/comnic/lesson/osunegi/pipi_where/dao/AccountDao.java
@@ -0,0 +1,42 @@
+package jp.co.comnic.lesson.osunegi.pipi_where.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import jp.co.comnic.lesson.osunegi.pipi_where.beans.Account;
+
+public class AccountDao {
+	public static Account findBy(String userName) throws DaoException{
+		return findBy(userName, null);
+	}
+	
+	public static Account findBy(String userName, String password)throws DaoException{
+		Account account = new Account();
+		String sql = "SELECT * FROM account WHERE user_name = ? ";
+		if(password != null) {
+			sql += " AND password = ?";
+		}
+		try (Connection conn = ConnectionFactory.getConnection();
+				 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				
+				pstmt.setString(1, userName);
+				if (password != null) {
+					pstmt.setString(2, password);
+				}
+				try (ResultSet rs = pstmt.executeQuery()) {
+					if (rs.next()) {
+						account.setUserName(rs.getString("user_name"));
+						account.setAuthenticated(true);
+					}
+				}
+			} catch (NamingException | SQLException e) {
+				throw new DaoException(e);
+			}
+		return account;
+	}
+}
+
